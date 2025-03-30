8c026d64e768700556212d70b6529f289af8df2c
radz22 <radzsantillan01@gmail.com>
2025-03-30 23:07:49 +0800

    implement direction of signin-hook

--- frontend/src/hook/signin-hook.jsx
+++ frontend/src/hook/signin-hook.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import { handleErrorAlert, handleSuccessAlert } from "../component/sweet-alert";
 import { signinServices } from "../services/authService";
+import { useNavigate } from "react-router-dom";
 const SignInHook = () => {
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleSignIn = async (credentials) => {
     setLoading(true);
     try {
       const data = await signinServices(credentials);
       handleSuccessAlert("Successfully signed in!");
+
+      setTimeout(() => {
+        navigate("/home");
+      }, 2000);
+
       return data;
     } catch (error) {
       handleErrorAlert(error.response.data.error);
