7d6858e58426e9fc7699468634cb5df6d5e533f0
Radz Santillan <116485181+radz22@users.noreply.github.com>
2025-04-03 15:40:54 +0800

    chore: added 11 to 25 level in html

---
+++ frontend/src/pages/css-question.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CssQuestion = () => {
+  return <div>CssQuestion</div>;
+};
--- frontend/src/pages/html-question.jsx
+++ frontend/src/pages/html-question.jsx
@@ -18,6 +18,21 @@ import Question7 from "../component/question-html/question7";
 import Question8 from "../component/question-html/question-8";
 import Question9 from "../component/question-html/question-9";
 import Question10 from "../component/question-html/question-10";
+import Question11 from "../component/question-html/question11";
+import Question12 from "../component/question-html/question12";
+import Question13 from "../component/question-html/question13";
+import Question14 from "../component/question-html/question14";
+import Question15 from "../component/question-html/question15";
+import Question16 from "../component/question-html/question16";
+import Question17 from "../component/question-html/question17";
+import Question18 from "../component/question-html/question18";
+import Question19 from "../component/question-html/question19";
+import Question20 from "../component/question-html/question20";
+import Question21 from "../component/question-html/question21";
+import Question22 from "../component/question-html/question22";
+import Question23 from "../component/question-html/question23";
+import Question24 from "../component/question-html/question24";
+import Question25 from "../component/question-html/question25";
 import { handleErrorAlert } from "../component/sweet-alert";
 import { useNavigate } from "react-router-dom";
 const HtmlQuestion = () => {
@@ -155,6 +170,126 @@ const HtmlQuestion = () => {
       case 10:
         return (
           <Question10
+            data={questionData}
+            handleAnswerChange={handleAnswerChange}
+            answerInput={answerInput}
+          />
+        );
+      case 11:
+        return (
+          <Question11
+            data={questionData}
+            handleAnswerChange={handleAnswerChange}
+            answerInput={answerInput}
+          />
+        );
+      case 12:
+        return (
+          <Question12
+            data={questionData}
+            handleAnswerChange={handleAnswerChange}
+            answerInput={answerInput}
+          />
+        );
+      case 13:
+        return (
+          <Question13
+            data={questionData}
+            handleAnswerChange={handleAnswerChange}
+            answerInput={answerInput}
+          />
+        );
+      case 14:
+        return (
+          <Question14
+            data={questionData}
+            handleAnswerChange={handleAnswerChange}
+            answerInput={answerInput}
+          />
+        );
+      case 15:
+        return (
+          <Question15
+            data={questionData}
+            handleAnswerChange={handleAnswerChange}
+            answerInput={answerInput}
+          />
+        );
+      case 16:
+        return (
+          <Question16
+            data={questionData}
+            handleAnswerChange={handleAnswerChange}
+            answerInput={answerInput}
+          />
+        );
+      case 17:
+        return (
+          <Question17
+            data={questionData}
+            handleAnswerChange={handleAnswerChange}
+            answerInput={answerInput}
+          />
+        );
+      case 18:
+        return (
+          <Question18
+            data={questionData}
+            handleAnswerChange={handleAnswerChange}
+            answerInput={answerInput}
+          />
+        );
+      case 19:
+        return (
+          <Question19
+            data={questionData}
+            handleAnswerChange={handleAnswerChange}
+            answerInput={answerInput}
+          />
+        );
+      case 20:
+        return (
+          <Question20
+            data={questionData}
+            handleAnswerChange={handleAnswerChange}
+            answerInput={answerInput}
+          />
+        );
+      case 21:
+        return (
+          <Question21
+            data={questionData}
+            handleAnswerChange={handleAnswerChange}
+            answerInput={answerInput}
+          />
+        );
+      case 22:
+        return (
+          <Question22
+            data={questionData}
+            handleAnswerChange={handleAnswerChange}
+            answerInput={answerInput}
+          />
+        );
+      case 23:
+        return (
+          <Question23
+            data={questionData}
+            handleAnswerChange={handleAnswerChange}
+            answerInput={answerInput}
+          />
+        );
+      case 24:
+        return (
+          <Question24
+            data={questionData}
+            handleAnswerChange={handleAnswerChange}
+            answerInput={answerInput}
+          />
+        );
+      case 25:
+        return (
+          <Question25
             data={questionData}
             handleAnswerChange={handleAnswerChange}
             answerInput={answerInput}
