fbc20d8838847e6559f2d0b0234d360b1f393355
radz22 <radzsantillan01@gmail.com>
2025-05-08 01:52:06 +0800

    chore: delete dist

--- backend/dist/routes/question-js-route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const question_answer_js_controller_1 = require("../controller/question-answer-js-controller");
-const questionJsRoute = (0, express_1.Router)();
-questionJsRoute.post("/create-question", question_answer_js_controller_1.createQuestion);
-questionJsRoute.get("/:category/:level", question_answer_js_controller_1.getByCategoryAndLevel);
-exports.default = questionJsRoute;
--- backend/dist/services/check-email-existing-.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkEmailExisting = void 0;
-const account_model_1 = require("../model/account-model");
-const checkEmailExisting = async (email) => {
-    const existingUser = await account_model_1.accountModel.findOne({ email });
-    return existingUser;
-};
-exports.checkEmailExisting = checkEmailExisting;
